ae75d0c23a8b2c5095e98ed6a1d756de671f1f49
Anatoly Weinstein <anatoly.wein@gmail.com>
2025-10-21 09:34:19 +0200

    add time and delta to program frame options

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -1,22 +1,31 @@
 #include "Frame.h"
 
-[[noreturn]] void printHelp(const char * progname) {
-    // TODO add meaningful help message
-    printf(
+/**
+ * @brief Print help message and exit
+ */
+[[noreturn]]
+void printHelp(const char * progname) {
+    fprintf(stderr, 
         "Usage:\n"
         "  %s [input] -o [output]\n\n"
         "Options:\n"
         "  -o, --output <dir>    output directory\n"
+        "  -t, --time <int>      total simulation time (default: 1000)\n"
+        "  -d, --delta <float>   time step delta (default: 0.014)\n"
         "  -h, --help            print this help message\n",
         progname
     );
     exit(1);
 }
 
-[[noreturn]] void printUsage(const char * progname) {
-    // TODO add meaningful help message
-    printf(
-        "Usage: %s [file] -o [dir]\tapply molecular simulation to input <file> and produce output in <dir>\n",
+/**
+ * @brief Print usage message and exit. A short version of print help
+ * used only when an error occurs in program frame.
+ */
+[[noreturn]]
+void printUsage(const char * progname) {
+    fprintf(stderr,
+        "Usage: %s [file] \t apply molecular simulation to input <file> and produce output in <dir>\n",
         progname
     );
     exit(1);
@@ -33,8 +42,16 @@ Args ProcessArgs(int argc, char *argv[]) {
         switch (opt)
         {
         case 'o':
+            // TODO input validation
             args.output_dir = optarg;
-            printf("Output directory set to: %s\n", args.output_dir);
+            break;
+        case 't':
+            // TODO input validation
+            args.end_time = atof(optarg);
+            break;
+        case 'd':
+            // TODO input validation
+            args.delta_t = atof(optarg);
             break;
 
         case 'h': // -h or --help
@@ -49,6 +66,9 @@ Args ProcessArgs(int argc, char *argv[]) {
     if (optind < argc) {
         args.input_file = argv[optind];
         optind++;
+    } else {
+        fprintf(stderr, "missing positional argument: input file");
+        printUsage(progname);
     }
 
     // if output directory is not set, use default
--- src/Frame.h
+++ src/Frame.h
@@ -7,13 +7,15 @@
 #include <getopt.h>
 #include <iostream>
 
-const char* const OPTSTRING = "ho:";
+const char* const OPTSTRING = "ho:t:d:";
 
 /**
  * Definition of all long options.
  */
 const option GETOPT_LONG[] = {
     {"output", required_argument, 0, 'o'},
+    {"time", required_argument, 0, 't'},
+    {"delta", required_argument, 0, 'd'},
     {"help", no_argument, 0, 'h'},
     {0, 0, 0, 0}
 };
@@ -26,11 +28,11 @@ struct Args {
     const char* output_dir;
 
     double start_time = 0;
-    double end_time = 100; // 1000
+    double end_time = 1000;
     double delta_t = 0.014;
 
     // constructor with default values
-    Args() : input_file(NULL), output_dir(NULL), start_time(0), end_time(100), delta_t(0.014) {}
+    Args() : input_file(NULL), output_dir(NULL), start_time(0), end_time(1000), delta_t(0.014) {}
 };
 
 const char* const DEFAULT_OUTPUT_DIR = "gen";
