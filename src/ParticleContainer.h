4654b3d71828f4fcb6c747249f2d3c8afaff7a9c
Anatoly Weinstein <anatoly.wein@gmail.com>
2025-10-30 11:48:21 +0100

    document particle container

--- src/ParticleContainer.h
+++ src/ParticleContainer.h
@@ -4,7 +4,6 @@
 
 #pragma once
 
-#include <functional>
 #include <vector>
 
 #include "Particle.h"
@@ -31,29 +30,80 @@ class ParticleContainer {
 
     explicit ParticleContainer(size_type capacity) { particles.reserve(capacity); }
 
+    /**
+     * @brief Begin non-const iterator for ParticleContainer.
+     * @note Allows range-based for loops.
+     * @example
+     * ```c++
+     * ParticleContainer container;
+     * 
+     * for (auto &particle : container) {
+     *     std::cout << particle.toString() << std::endl;
+     * }
+     * ```
+     */
     iterator begin() { return particles.begin(); }
+
+    /**
+     * @brief End non-const iterator for ParticleContainer.
+     * @note Allows range-based for loops.
+     */
     iterator end() { return particles.end(); }
+
+    /**
+     * @brief Begin const iterator for ParticleContainer.
+     * @note Allows range-based for loops over constant elements.
+     * @example
+     * ```c++
+     * ParticleContainer container;
+     * 
+     * for (const auto &particle : container) {
+     *     std::cout << particle.toString() << std::endl;
+     * }
+     * ```
+     */
     const_iterator begin() const { return particles.begin(); }
+
+    /**
+     * @brief End const iterator for ParticleContainer.
+     * @note Allows range-based for loops.
+     */
     const_iterator end() const { return particles.end(); }
 
+    /**
+     * @brief Clear all particles from the container.
+     */
     void clear() { particles.clear(); }
-    void reserve(size_type capacity) { particles.reserve(capacity); }
 
+    /**
+     * @brief Get the number of particles in the container.
+     */
     size_type size() const { return particles.size(); }
+
+    /**
+     * @brief Get the capacity of the undderlying vector in the container.
+     */
     size_type capacity() const { return particles.capacity(); }
 
+    /**
+     * @brief Add a new Particle to the container.
+     */
     void emplace_back(const Vec3D &position, const Vec3D &velocity, double mass, int type = 0) {
         particles.emplace_back(position, velocity, mass, type);
+    }
+
+    /**
+     * @brief Add a new Particle to the container.
+     */
+    void emplace_back(const Particle &particle) {
+        particles.emplace_back(particle);
     }
 
     /**
      * @brief Reserve memory for particles.
      * @param reserve Number of particles to reserve space for.
      */
-    void reserve(int reserve = 0) { particles.reserve(reserve); }
-
-    void pushback(const Particle &p) { particles.push_back(p); }
-    void pushback(Particle &&p) { particles.emplace_back(std::move(p)); }
+    void reserve(size_type reserve = 0) { particles.reserve(reserve); }
 
     /**
      * @brief Iteration over single particles.
@@ -68,7 +118,7 @@ class ParticleContainer {
     //  * @brief Reduction of an accumulator value, processing over all single particles.
     //  */
     // template<typename Acc>
-    // void reduce(const std::function<Acc(Particle &, Acc)> &callback, Acc acc) {
+    // void reduce(const std::function<Acc(Particle &, Acc)> &callback, Acc acc = default) {
     //     for (size_t i = 0; i < particles.size(); i++) {
     //         acc = callback(particles[i], acc);
     //     }
