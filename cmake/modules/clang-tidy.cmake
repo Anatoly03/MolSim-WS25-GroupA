532af03822500b6ddc78bbea2da3799a120edd9a
Anatoly Weinstein <anatoly.wein@gmail.com>
2025-11-04 12:12:05 +0100

    disable clang-tidy in build

--- cmake/modules/clang-tidy.cmake
+++ cmake/modules/clang-tidy.cmake
@@ -4,8 +4,9 @@ find_program(CLANG_TIDY "clang-tidy")
 if(CLANG_TIDY)
     message(STATUS "Found clang-tidy: ${CLANG_TIDY}")
 
+    # TODO fix clang-tidy
     # set clang-tidy as static analyzer if found
-    set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY} src/MolSim.cpp -p build")
+    # set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY} src/MolSim.cpp -p build")
 
     # Create tidy target
     add_custom_target(tidy
