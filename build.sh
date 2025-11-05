007ff70b6266751af8c4183a1f3b1d232040c545
Anatoly Weinstein <anatoly.wein@gmail.com>
2025-11-05 16:41:37 +0100

    clear build dir in build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # # Clean previous build directory if it exists
-# if [ -d "build" ]; then rm -rf build; fi
+if [ -d "build" ]; then rm -rf build; fi
 mkdir build
 
 # Build CMake
