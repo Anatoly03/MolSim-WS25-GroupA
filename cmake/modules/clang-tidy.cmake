24f3c17aa7823cdd33ef7cefe658c6aaa465cceb
Anatoly Weinstein <anatoly.wein@gmail.com>
2025-11-06 15:15:34 +0100

    change clang-tidy

--- cmake/modules/clang-tidy.cmake
+++ cmake/modules/clang-tidy.cmake
@@ -4,13 +4,13 @@ find_program(CLANG_TIDY "clang-tidy")
 if(CLANG_TIDY)
     message(STATUS "Found clang-tidy: ${CLANG_TIDY}")
 
-    # TODO fix clang-tidy
     # set clang-tidy as static analyzer if found
-    # set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY} src/MolSim.cpp -p build")
+    set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY} src/app/MolSim.cpp -p build")
 
     # Create tidy target
     add_custom_target(tidy
-            COMMAND ${CLANG_TIDY} src/MolSim.cpp -p build --fix
+            COMMAND ${CMAKE_CXX_CLANG_TIDY} --fix
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
             COMMENT "Running clang-tidy (static analysis)"
     )
 else()
