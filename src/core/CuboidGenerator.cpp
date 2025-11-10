2ef4c5361955ba16236c04ebae0e06ba4df4f001
zhengyingzhao <ge95cuz@mytum.de>
2025-11-10 21:53:07 +0100

    implement task4 the Cuboid Generator with Brownian velocity

---
+++ src/core/CuboidGenerator.cpp
@@ -0,0 +1,34 @@
+//
+// Created by zhengying zhao on 10.11.25.
+//
+
+#include "CuboidGenerator.h"
+#include "utils/MaxwellBoltzmannDistribution.h"
+
+/**
+ * @brief Adds a cuboid of particles to the container and applies Brownian initialization.
+ *
+ * This function uses a triple loop to simulate a regular 3D grid of particles.
+ * A random Brownian velocity is sampled using maxwellBoltzmannDistributedVelocity().
+ *
+ * The Brownian velocity is added once to the cuboid's initial velocity,
+ * so that Brownian motion is applied only at initialization.
+ */
+void addCuboid(ParticleContainer &particles,
+               const Cuboid &cuboid,
+               double brownian_sigma) {
+    particles.reserve(particles.size() + cuboid.n1 * cuboid.n2 * cuboid.n3);
+
+    for (int i = 0; i < cuboid.n1; ++i) {
+        for (int j = 0; j < cuboid.n2; ++j) {
+            for (int k = 0; k < cuboid.n3; ++k) {
+                Vec3D pos = cuboid.position + Vec3D(i * cuboid.h, j * cuboid.h, k * cuboid.h);
+                Vec3D vel = cuboid.initial_velocity;
+
+                if (brownian_sigma > 0.0) vel += maxwellBoltzmannDistributedVelocity(brownian_sigma, 3);
+
+                particles.emplace_back(pos, vel, cuboid.mass);
+            }
+        }
+    }
+}
