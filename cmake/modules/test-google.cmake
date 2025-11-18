b7e5f619d80d6017fd656943c6aba4abe075fa7d
Anatoly Weinstein <anatoly.wein@gmail.com>
2025-11-18 14:55:41 +0100

    set googletest git tag

--- cmake/modules/test-google.cmake
+++ cmake/modules/test-google.cmake
@@ -21,7 +21,7 @@ include(FetchContent)
 FetchContent_Declare(
     googletest
     GIT_REPOSITORY    https://github.com/google/googletest.git
-    GIT_TAG           main
+    GIT_TAG           1.17.0
 )
 
 # windows: prevent overriding the parent project's compiler/linker settings
