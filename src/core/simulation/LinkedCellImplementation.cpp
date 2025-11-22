15e654c1bee3235876af2676c6ea7f1ef6665f1e
Haoyuan Ma <mgerichyma@gmail.com>
2025-11-22 18:41:22 +0100

    starting implementation of Linked Cells

--- src/core/simulation/LinkedCellImplementation.cpp
+++ src/core/simulation/LinkedCellImplementation.cpp
@@ -1,2 +1,14 @@
 
+#include "../Args.h"
+#include "Simulation.h"
+
+class LinkedCellImplementation : public Simulation {
+
+    /**
+     * @brief Advance the simulation by one time step.
+     */
+    void tick() override {
+        // TODO work here
+    }
+};
 // TODO work here
--- src/core/simulation/LinkedCellImplementation.h
+++ src/core/simulation/LinkedCellImplementation.h
@@ -4,21 +4,28 @@
 #include "../Args.h"
 #include "Simulation.h"
 
-class DirectSumAlgorithm : public Simulation {
+class LinkedCellImplementation : public Simulation {
    private:
     ParticleContainer& particles;
+    std::vector<std::list<Particle>> cells;
+    int cellSize;
 
    public:
     /**
      * @note Default constructor without providing particle container is private.
      */
-    DirectSumAlgorithm() = delete;
+    LinkedCellImplementation() = delete;
     // DirectSumAlgorithm() : arguments(), particles(), writer(particles) {}
 
     /**
      * @brief Default constructor
      */
-    DirectSumAlgorithm(ParticleContainer &p, const Args &args) : Simulation(args), particles(p) {}
+    LinkedCellImplementation(const Args &args, ParticleContainer &p,int cuttOff, int xOfDomain, int yOfDomain, int zOfDomain) : Simulation(args), particles(p){
+        cells.resize((xOfDomain/cuttOff) * (yOfDomain/cuttOff) * (zOfDomain/cuttOff));
+        cellSize=cuttOff;
+        placeInCells();
+
+    }
 
    public:
     /**
@@ -26,5 +33,23 @@ class DirectSumAlgorithm : public Simulation {
      */
     void tick() override {
         // TODO work here
+    }
+
+    /**
+     * @brief places all particles into correct cells.
+     */
+    void placeInCells(){
+
+        particles.forEach([&](Particle &p) {
+            int cx = static_cast<int>(p.getPosition().x / cellSize);
+            int cy = static_cast<int>(p.getPosition().y / cellSize);
+            int cz = static_cast<int>(p.getPosition().z / cellSize);
+
+            int index = cz * (p.getPosition().x * p.getPosition().y) + cy * p.getPosition().x + cx;
+            cells[index].push_back(p);
+        });
+
+
+
     }
 };
