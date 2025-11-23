b003c097fe7b99c0ebe8b49c7779daa89fb33893
Haoyuan Ma <mgerichyma@gmail.com>
2025-11-23 19:09:04 +0100

    combined implementation of Linked Cells

--- src/core/simulation/LinkedCellImplementation.cpp
+++ src/core/simulation/LinkedCellImplementation.cpp
@@ -10,16 +10,112 @@
  */
 void LinkedCellImplementation::placeInCells(ParticleContainer &particles) {
     particles.forEach([&](Particle &p) {
-        Vec3<int> cellIndex;
+        int cx = static_cast<int>(p.getPosition().x / cellSize);
+        int cy = static_cast<int>(p.getPosition().y / cellSize);
+        int cz = static_cast<int>(p.getPosition().z / cellSize);
 
-        cellIndex.x = (int)(p.position.x) / cellSize.x;
-        cellIndex.y = (int)(p.position.y) / cellSize.y;
-        cellIndex.z = (int)(p.position.z) / cellSize.z;
+        int index = cz * (p.getPosition().x * p.getPosition().y) + cy * p.getPosition().x + cx;
+        cells[index].push_back(p);
+    });
 
-        // if (cells[cellIndex] == nullptr) {
-        //     cells[cellIndex] = std::vector<Particle>();
-        // }
 
-        cells[cellIndex].push_back(p);
+
+}
+std::vector<Particle*> LinkedCellImplementation::boundaryParticles() {
+    std::vector<Particle*> result;
+    for (int i = 1; i < nx-1; ++i) {
+        for (int j = 1; j < ny-1; ++j) {
+            for (int k = 1; k < nz-1; ++k) {
+                if (i == 1 || i == nx-2 ||
+                    j == 1 || j == ny-2 ||
+                    k == 1 || k == nz-2) {
+                    for (auto &p : cells[i + nx * (j + ny * k)]) {
+                        result.push_back(&p);
+                    }
+
+                }
+            }
+        }
+    }
+    return result;
+}
+
+/**
+ * @brief calculate the position for all particles
+ */
+void LinkedCellImplementation::calculatePosition() {
+    const double dt = arguments.delta_t;
+
+    particles.forEach([dt](Particle &particle) {
+        Vec3D x = particle.getPosition() + dt * particle.getVelocity() +
+                  std::pow(dt, 2) * particle.getForce() / (2 * particle.getMass());
+        particle.setPosition(x);
+    });
+}
+
+/**
+ * @brief calculate the velocity for all particles
+ */
+void LinkedCellImplementation::calculateVelocity() {
+    const double dt = arguments.delta_t;
+
+    particles.forEach([dt](Particle &particle) {
+        Vec3D v =
+                particle.getVelocity() + dt * ((particle.getForce() + particle.getOldForce()) / (2 * particle.getMass()));
+        particle.setVelocity(v);
+    });
+}
+
+/**
+ * @brief delay the force for all particles
+ */
+void LinkedCellImplementation::delayForce() {
+    particles.forEach([](Particle &particle) {
+        particle.delayForce();
+    });
+}
+std::vector<Particle*> LinkedCellImplementation::innerParticles() {
+    std::vector<Particle*> result;
+    for (int i = 1; i < nx-1; ++i) {
+        for (int j = 1; j < ny-1; ++j) {
+            for (int k = 1; k < nz-1; ++k) {
+                if (!(i == 1 || i == nx-2 ||
+                    j == 1 || j == ny-2 ||
+                    k == 1 || k == nz-2)) {
+                    for (auto &p : cells[i + nx * (j + ny * k)]) {
+                        result.push_back(&p);
+                    }
+
+                }
+            }
+        }
+    }
+    return result;
+}
+
+void LinkedCellImplementation::calculateForce() {
+    // Lennard-Jones Parameters (hard code for now)
+    const double epsilon = 5.0;
+    const double sigma   = 1.0;
+
+    particles.forEachDistinctPair([&](Particle &p_i, Particle &p_j) {
+        Vec3D r = p_i.getPosition() - p_j.getPosition();
+
+        double r_len = r.length();
+        if (r_len == 0.0) {
+            return;
+        }
+        if(r_len>cutOff){
+            return;
+        }
+        // rf. formula (3)
+        double inv_r2 = 1.0 / (r_len * r_len);
+        double sigma_r2 = sigma * inv_r2;
+        double scalar = -24.0 * epsilon * std::pow(inv_r2, 2) *
+                        ( std::pow(sigma_r2, 6) - 2.0 * std::pow(sigma_r2, 12) );
+        Vec3D F_ij = scalar * r;
+
+        p_i.addForce(F_ij);
+        p_j.addForce(-F_ij);
     });
 }
--- src/core/simulation/LinkedCellImplementation.h
+++ src/core/simulation/LinkedCellImplementation.h
@@ -9,32 +9,38 @@
 
 class LinkedCellImplementation : public Simulation {
    private:
+    ParticleContainer& particles;
+    std::vector<std::list<Particle>> cells;
+    int cellSize;
+    int cutOff;
+
     std::map<Vec3<int>, std::vector<Particle>> cells;
-    
+
     Vec3D cellSize = Vec3D(1.0);
 
    public:
+    int nx=0;
+    int ny=0;
+    int nz=0;
     /**
      * @note Default constructor without providing particle container is private.
      */
     LinkedCellImplementation() = delete;
+    // DirectSumAlgorithm() : arguments(), particles(), writer(particles) {}
 
     /**
-     * @brief Default constructor
+     * @brief the +2 contribute to creating ghost cells which helps with boundery
      */
-    LinkedCellImplementation(ParticleContainer &p, const Args &args) : Simulation(args), cells() {
-        // TODO work here
+    LinkedCellImplementation(const Args &args, ParticleContainer &p,int size, int xOfDomain, int yOfDomain, int zOfDomain,int cutOff) : Simulation(args), particles(p) {
 
-        // int cuttOff = 0;
-        // int xOfDomain = 0;
-        // int yOfDomain = 0;
-        // int zOfDomain = 0;
+        nx = static_cast<int>(std::ceil(xOfDomain / static_cast<double>(size))) + 2;
+        ny = static_cast<int>(std::ceil(yOfDomain / static_cast<double>(size))) + 2;
+        nz = static_cast<int>(std::ceil(zOfDomain / static_cast<double>(size))) + 2;
 
-        // cells.resize((xOfDomain/cuttOff) * (yOfDomain/cuttOff) * (zOfDomain/cuttOff));
-        // cellSize=cuttOff;
-
-        // Flood the cells with particles from the container.
+        cells.resize(nx * ny * nz);
+        cellSize=size;
         placeInCells(p);
+        this->cutOff=cutOff;
     }
 
    private:
@@ -43,11 +49,48 @@ class LinkedCellImplementation : public Simulation {
      */
     void placeInCells(ParticleContainer &p);
 
-   public:
+    /**
+    * @brief return all boundary particles .
+    */
+    std::vector<Particle*> boundaryParticles();
+
+
+    /**
+    * @brief return all inner particles .
+    */
+    std::vector<Particle*> innerParticles();
+
+    /**
+      * @brief calculate the position for all particles
+      */
+    void calculatePosition();
+
+    /**
+     * @brief calculate the velocity for all particles
+     */
+    void calculateVelocity();
+
+    /**
+     * @brief delay the force for all particles
+     */
+    void delayForce();
+
+    /**
+     * @brief calculate the force for all particles
+     */
+    void calculateForce();
+
+public:
     /**
      * @brief Advance the simulation by one time step.
      */
     void tick() override {
         // TODO work here
+        //calculatePosition();
+        //we should then first check the boundary cells and then the inner cells
+        delayForce();
+        calculateForce();
+        calculateVelocity();
+
     }
 };
