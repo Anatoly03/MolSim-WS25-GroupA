3b6d1631ba6a4a9473fa521e37974e1435312eca
Anatoly Weinstein <anatoly.wein@gmail.com>
2025-11-26 14:20:24 +0100

    remove trace log

--- src/core/LinkedCells.cpp
+++ src/core/LinkedCells.cpp
@@ -206,7 +206,8 @@ void LinkedCells::reindex() {
                 if (!ascending(domainMin.x, newCellIndex.x, domainMax.x)
                  || !ascending(domainMin.y, newCellIndex.y, domainMax.y)
                  || !ascending(domainMin.z, newCellIndex.z, domainMax.z)) {
-                    spdlog::trace("Particle left cell domain {} -> {}", currentCellIndex, newCellIndex);
+                    // the line below breaks github CI
+                    // spdlog::trace("Particle left cell domain {} -> {}", currentCellIndex, newCellIndex);
                     // spdlog::debug("Cell size {}, Current cell {}, New cell {}", cellSize, currentCellIndex, newCellIndex);
                     continue;
                 }
