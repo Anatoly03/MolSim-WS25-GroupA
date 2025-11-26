a0898c20aa74b577ab6a8aec0ed9eb796c010159
Anatoly Weinstein <anatoly.wein@gmail.com>
2025-11-26 22:08:13 +0100

    fix github ci

--- src/core/LinkedCells.cpp
+++ src/core/LinkedCells.cpp
@@ -211,7 +211,7 @@ void LinkedCells::reindex() {
                 || !ascending(domainMin.y, newCellIndex.y, domainMax.y)
                 || !ascending(domainMin.z, newCellIndex.z, domainMax.z)) {
                 // the line below breaks github CI
-                spdlog::warn("Particle {} at {} left cell domain {} -> {}", p.p_id, p.position, currentCellIndex, newCellIndex);
+                // spdlog::warn("Particle {} at {} left cell domain {} -> {}", p.p_id, p.position, currentCellIndex, newCellIndex);
                 // spdlog::debug("Cell size {}, Current cell {}, New cell {}", cellSize, currentCellIndex, newCellIndex);
                 continue;
             }
