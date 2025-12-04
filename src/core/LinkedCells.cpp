7fa7b57670861e03f364e32f028098d899abaa3f
Haoyuan Ma <mgerichyma@gmail.com>
2025-12-04 18:54:16 +0100

    adding customizable boundaries

--- src/core/LinkedCells.cpp
+++ src/core/LinkedCells.cpp
@@ -139,63 +139,111 @@ void LinkedCells::forEachBordered(const std::function<void(Particle &, Vec3I)> &
     const auto domainSize = domainMax - domainMin + Vec3I(1);
 
     // XY PLANE [FRONT]
-    for (auto xyPlane : Vec3Iter(domainSize.x, domainSize.y, 1)) {
-        Vec3I cellIndex = domainMin + Vec3I(xyPlane.x, xyPlane.y, 0);
-        auto &particles = containers[cellIndex];
+    if(boarderZmin=="reflect") {
+        for (auto xyPlane: Vec3Iter(domainSize.x, domainSize.y, 1)) {
+            Vec3I cellIndex = domainMin + Vec3I(xyPlane.x, xyPlane.y, 0);
+            auto &particles = containers[cellIndex];
 
-        for (auto &p: particles) {
-            callback(p, cellIndex+Vec3I(0, 0, -1));
+            for (auto &p: particles) {
+                callback(p, cellIndex + Vec3I(0, 0, -1));
+            }
         }
+    }else{
+        /*for (auto xyPlane: Vec3Iter(domainSize.x, domainSize.y, 1)) {
+            Vec3I cellIndex = domainMin + Vec3I(xyPlane.x, xyPlane.y, 0);
+            containers.erase(cellIndex);
+        }*/
+        clearOutOfBoundsCells();
     }
 
     // XY PLANE [BACK]
-    for (auto xyPlane : Vec3Iter(domainSize.x, domainSize.y, 1)) {
-        Vec3I cellIndex = domainMin + Vec3I(xyPlane.x, xyPlane.y, domainSize.z - 1);
-        auto &particles = containers[cellIndex];
+    if(boarderZmax == "reflect") {
+        for (auto xyPlane: Vec3Iter(domainSize.x, domainSize.y, 1)) {
+            Vec3I cellIndex = domainMin + Vec3I(xyPlane.x, xyPlane.y, domainSize.z - 1);
+            auto &particles = containers[cellIndex];
 
-        for (auto &p: particles) {
-            callback(p, cellIndex+Vec3I(0, 0, 1));
+            for (auto &p: particles) {
+                callback(p, cellIndex + Vec3I(0, 0, 1));
+            }
         }
+    }else{
+        /*for (auto xyPlane: Vec3Iter(domainSize.x, domainSize.y, 1)) {
+            Vec3I cellIndex = domainMin + Vec3I(xyPlane.x, xyPlane.y, domainSize.z - 1);
+            containers.erase(cellIndex);
+        }*/
+        clearOutOfBoundsCells();
     }
 
     // XZ PLANE [NORTH]
-    for (auto xyPlane : Vec3Iter(domainSize.x, 1, domainSize.z)) {
-        Vec3I cellIndex = domainMin + Vec3I(xyPlane.x, 0, xyPlane.z);
-        auto &particles = containers[cellIndex];
+    if(boarderYmin=="reflect") {
+        for (auto xyPlane: Vec3Iter(domainSize.x, 1, domainSize.z)) {
+            Vec3I cellIndex = domainMin + Vec3I(xyPlane.x, 0, xyPlane.z);
+            auto &particles = containers[cellIndex];
 
-        for (auto &p: particles) {
-            callback(p, cellIndex+Vec3I(0, -1, 0));
+            for (auto &p: particles) {
+                callback(p, cellIndex + Vec3I(0, -1, 0));
+            }
         }
+    }else{
+        /*for (auto xyPlane: Vec3Iter(domainSize.x, 1, domainSize.z)) {
+            Vec3I cellIndex = domainMin + Vec3I(xyPlane.x, 0, xyPlane.z);
+            containers.erase(cellIndex);
+        }*/
+        clearOutOfBoundsCells();
     }
 
     // XZ PLANE [SOUTH]
-    for (auto xyPlane : Vec3Iter(domainSize.x, 1, domainSize.z)) {
-        Vec3I cellIndex = domainMin + Vec3I(xyPlane.x, domainSize.y - 1, xyPlane.z);
-        auto &particles = containers[cellIndex];
+    if(boarderYmax=="reflect") {
+        for (auto xyPlane: Vec3Iter(domainSize.x, 1, domainSize.z)) {
+            Vec3I cellIndex = domainMin + Vec3I(xyPlane.x, domainSize.y - 1, xyPlane.z);
+            auto &particles = containers[cellIndex];
 
-        for (auto &p: particles) {
-            callback(p, cellIndex+Vec3I(0, 1, 0));
+            for (auto &p: particles) {
+                callback(p, cellIndex + Vec3I(0, 1, 0));
+            }
         }
+    }else{
+        /*for (auto xyPlane: Vec3Iter(domainSize.x, 1, domainSize.z)) {
+            Vec3I cellIndex = domainMin + Vec3I(xyPlane.x, domainSize.y - 1, xyPlane.z);
+            containers.erase(cellIndex);
+        }*/
+        clearOutOfBoundsCells();
     }
 
     // YZ PLANE [WEST]
-    for (auto xyPlane : Vec3Iter(1, domainSize.y, domainSize.z)) {
-        Vec3I cellIndex = domainMin + Vec3I(0, xyPlane.y, xyPlane.z);
-        auto &particles = containers[cellIndex];
+    if(boarderXmin=="reflect") {
+        for (auto xyPlane: Vec3Iter(1, domainSize.y, domainSize.z)) {
+            Vec3I cellIndex = domainMin + Vec3I(0, xyPlane.y, xyPlane.z);
+            auto &particles = containers[cellIndex];
 
-        for (auto &p: particles) {
-            callback(p, cellIndex+Vec3I(-1, 0, 0));
+            for (auto &p: particles) {
+                callback(p, cellIndex + Vec3I(-1, 0, 0));
+            }
         }
+    }else{
+        /*for (auto xyPlane: Vec3Iter(1, domainSize.y, domainSize.z)) {
+            Vec3I cellIndex = domainMin + Vec3I(0, xyPlane.y, xyPlane.z);
+            containers.erase(cellIndex);
+        }*/
+        clearOutOfBoundsCells();
     }
 
     // YZ PLANE [EAST]
-    for (auto xyPlane : Vec3Iter(1, domainSize.y, domainSize.z)) {
-        Vec3I cellIndex = domainMin + Vec3I(domainSize.x - 1, xyPlane.y, xyPlane.z);
-        auto &particles = containers[cellIndex];
+    if(boarderXmax=="reflect") {
+        for (auto xyPlane: Vec3Iter(1, domainSize.y, domainSize.z)) {
+            Vec3I cellIndex = domainMin + Vec3I(domainSize.x - 1, xyPlane.y, xyPlane.z);
+            auto &particles = containers[cellIndex];
 
-        for (auto &p: particles) {
-            callback(p, cellIndex+Vec3I(1, 0, 0));
+            for (auto &p: particles) {
+                callback(p, cellIndex + Vec3I(1, 0, 0));
+            }
         }
+    }else{
+        /*for (auto xyPlane : Vec3Iter(1, domainSize.y, domainSize.z)) {
+            Vec3I cellIndex = domainMin + Vec3I(domainSize.x - 1, xyPlane.y, xyPlane.z);
+            containers.erase(cellIndex);
+        }*/
+        clearOutOfBoundsCells();
     }
 }
 
--- src/core/reader/YamlReader.h
+++ src/core/reader/YamlReader.h
@@ -170,6 +170,16 @@ class YamlReader : public FileReader {
         const double cut_off = unwrap_node<double>(args.cutoff_radius, "config", "cut_off");
         const std::string attraction_method = unwrap_node<std::string>("lennard-jones", "config", "attraction");
 
+        const std::string boarderXmin = unwrap_node<std::string>("reflect", "config", "delete");
+        const std::string boarderXmax = unwrap_node<std::string>("reflect", "config", "delete");
+        const std::string boarderYmin = unwrap_node<std::string>("reflect", "config", "delete");
+        const std::string boarderYmax = unwrap_node<std::string>("reflect", "config", "delete");
+        const std::string boarderZmin = unwrap_node<std::string>("reflect", "config", "delete");
+        const std::string boarderZmax = unwrap_node<std::string>("reflect", "config", "delete");
+
+
+
+
         if (args.delta_t_cli) {
             spdlog::warn("delta_time in {} overridden by CLI argument: {} -> {}", args.input_file, delta_time, args.delta_t);
         } else {
@@ -196,6 +206,13 @@ class YamlReader : public FileReader {
         args.sigma = sigma;
         args.cutoff_radius = cut_off;
         args.attraction_method = attraction_method;
+
+        args.boarderXmin = boarderXmin;
+        args.boarderXmax = boarderXmax;
+        args.boarderYmin = boarderYmin;
+        args.boarderYmax = boarderYmax;
+        args.boarderZmin = boarderZmin;
+        args.boarderZmax = boarderZmax;
     }
 
     /**
