0a7dd90674ab771d57642b290e423548cf39a3b8
Anatoly Weinstein <anatoly.wein@gmail.com>
2026-01-28 12:06:05 +0100

    fix merge conflict

--- build.sh
+++ build.sh
@@ -6,11 +6,7 @@ mkdir build
 
 # Build CMake
 export CMAKE_POLICY_VERSION_MINIMUM=3.5
-<<<<<<< HEAD
-cmake -S . -B build -DENABLE_TRACY=OFF -DENABLE_VTK_OUTPUT=OFF -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON -Wno-dev
-=======
 cmake -S . -B build -DENABLE_TRACY=ON -DENABLE_VTK_OUTPUT=OFF -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON -Wno-dev
->>>>>>> 04b44ee (cmake add openmp support)
 
 # Build project
 make -C build -j4
