0e78818c073e0254fac10897a6659acd48500528
Anatoly Weinstein <anatoly.wein@gmail.com>
2026-01-28 12:41:22 +0100

    fix reorder (github ci)

--- src/core/simulation/Simulation.h
+++ src/core/simulation/Simulation.h
@@ -75,7 +75,7 @@ class Simulation {
     /**
      * @brief Default constructor
      */
-    Simulation(ParticleContainer &p, const Args &args) : arguments(args), particles(p) {
+    Simulation(ParticleContainer &p, const Args &args) : particles(p), arguments(args) {
         // use the attraction provided by args
         forceCalculationSystem = get_force_system_by_name(args.attraction_method);
         forceCalculationEnabled = (args.attraction_method != "null");
